a16033d19b9945d75c740f13a2bb811acc61c78d
Andres Coria <andrescoria1984@gmail.com>
2023-03-24 23:46:54 -0300

    segundo desafio, anexo el primer desafio

--- desafio-1/primerDesafio.js
+++ desafio-1/primerDesafio.js
@@ -1,13 +1,14 @@
 class ProductManager {
     constructor() {
-        this.products = []
+        this.path = path
     }
-    addProducts = (title, description, price, thumbnail, stock) => {
+    addProduct = (title, description, price, thumbnail, code, stock) => {
         const product = {
             title,
             description,
             price,
             thumbnail,
+            code,
             stock
         }
         if(this.products.length === 0){
@@ -15,6 +16,9 @@ class ProductManager {
         }else{
             product.id = this.products[this.products.length - 1].id +1
         }
+        let newProduct = this.products.find(prod => prod.code === product.code)
+        if (newProduct) return console.log('Un producto con ese code ya fue ongresado')
+
         if (Object.values(product).every(value => value)) {
             this.products.push(product);
         } else {
@@ -24,17 +28,17 @@ class ProductManager {
     getProducts = () => {
         return this.products
     }
-    getProductsById = ( id ) => {
+    getProductById = ( id ) => {
         const obj = this.products.find(product => product.id === id);
         return obj ? obj : console.log('No products found');
     }
 }
 
 const aplicacion = new ProductManager()
-aplicacion.addProducts('Cs 1.6','Servidor de calidad media cs 1.6', 500, 'thumnbail', 15);
-aplicacion.addProducts('Cs 1.6', 'Servidor de calidad alta cs 1.6', 1000, 'thumbnail', 10);
-aplicacion.addProducts('Cs Go', 'Servidor de CSGO medio', 850, 'thumnail', 15);
-aplicacion.addProducts('Cs Go', 'Servidor de CSGO alto', 950, 'thumnail', 15);
-aplicacion.addProducts('Cs Go', 'Servidor de CSGO bajo', 750, 'thumnail', 15);
-console.log('Muestra solo el producto con id: 2 ', aplicacion.getProductsById(2))
+aplicacion.addProduct('Cs 1.6','Servidor de calidad media cs 1.6', 500, 'thumnbail', 001, 15);
+aplicacion.addProduct('Cs 1.6', 'Servidor de calidad alta cs 1.6', 1000, 'thumbnail', 002, 10);
+// aplicacion.addProduct('Cs Go', 'Servidor de CSGO medio', 850, 'thumnail', 15);
+// aplicacion.addProduct('Cs Go', 'Servidor de CSGO alto', 950, 'thumnail', 15);
+// aplicacion.addProduct('Cs Go', 'Servidor de CSGO bajo', 750, 'thumnail', 15);
+// console.log('Muestra solo el producto con id: 2 ', aplicacion.getProductById(2))
 console.log('Muestra todos los productos agregados ', aplicacion.getProducts())
---
+++ desafio-2/test.js
@@ -0,0 +1,66 @@
+const ProductManager = require ('./app.js')
+const products = new ProductManager ('./products.json')
+
+async function Test() {
+
+  const objeto1 = {
+    title: "Cs 1.6",
+    description: 'Servidor de calidad media cs 1.6',
+    price: 800,
+    thumbnail: "url",
+    code: 1,
+    stock: 15
+  }
+
+  const objeto2 = {
+    title: "Cs 1.6",
+    description: 'Servidor de calidad baja cs 1.6',
+    price: 500,
+    thumbnail: "url",
+    code: 2,
+    stock: 14
+  }
+
+  const objeto3 = {
+    title: "Cs 1.6",
+    description: 'Servidor de calidad media cs 1.6',
+    price: 1000,
+    thumbnail: "url",
+    code: 3,
+    stock: 12
+  }
+
+  const objeto4 = {
+    title: "Cs 1.6",
+    description: 'Servidor de calidad alta cs 1.6',
+    price: 1200,
+    thumbnail: "url",
+    code: 4,
+    stock: 15
+  }
+
+  const upDateObject = {
+    id: 1,
+    title: "Cs GO",
+    description: 'Servidor de calidad alta Global Ofenssive',
+    price: 1800,
+    thumbnail: "url",
+    code: 4,
+    stock: 5
+  }
+
+  await products.addProduct(objeto1).then(data => console.log(data));
+  // await products.getProducts().then(data => console.log(data));
+  // await products.getProductById(2).then(data => console.log(data));
+  // await products.updateProduct(3, {
+  //   "title": "CS GO",
+  //   "description": 'Servidor de calidad alta Global Ofenssive',
+  //   "price": 1800,
+  //   "thumbnail": "url",
+  //   "code": 4,
+  //   "stock": 5,
+  // })
+  // await products.deleteById(2)
+}
+
+Test();
