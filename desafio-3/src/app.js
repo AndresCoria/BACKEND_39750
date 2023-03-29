b3021207c06cdfc3f3cb0af58ed29ba62bca0549
Andres Coria <andrescoria1984@gmail.com>
2023-03-29 17:42:04 -0300

    agregado try/catch en metodo get

--- desafio-3/src/app.js
+++ desafio-3/src/app.js
@@ -17,17 +17,25 @@ server.on('error', (error) => {
 const products = new Container('./products.json')
 
 app.get("/products", async (req, res) => {
-    const productsList = await products.getProducts()
-    const limit =  req.query.limit
-    if(!limit) return res.send(productsList)
-    res.send(productsList.slice(0, limit))
+    try {
+        const productsList = await products.getProducts()
+        const limit =  req.query.limit
+        if(!limit) return res.send(productsList)
+        res.send(productsList.slice(0, limit))
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 app.get("/products/:pid", async (req, res) => {
-    const id = parseInt(req.params.pid)
-    const product = await products.getProductById(id)
-    if(!product) res.send( '<h1 style="color: red">PRODUCT NOT FOUND</h1>')
-    res.send(product)
+    try {
+        const id = parseInt(req.params.pid)
+        const product = await products.getProductById(id)
+        if(!product) res.send( '<h1 style="color: red">PRODUCT NOT FOUND</h1>')
+        res.send(product)
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 
